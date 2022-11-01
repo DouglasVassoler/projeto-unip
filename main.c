3ab09991d59e4cff3593c6c98659fa03dbd1d2c5
Douglas Vassoler <vassoler04@gmail.com>
2022-11-01 15:53:30 -0300

    inicio do projeto

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+#include "string.h"
+#include "windows.h"
+#include "conio.h"
+#include "telainicial.h"
+#include "telalogin.h"
+
+int main()
+{
+ telainicial();
+
+ telalogin();
+
+
+ return 0;
+}
